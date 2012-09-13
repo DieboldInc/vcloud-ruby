4652720f8c356b92b505afc71cf4b609a1ca3ac1
Zach Robinson <robinsz@diebold.com>
2012-09-13 14:32:11 -0400

    create constants

---
+++ lib/vcloud/constants.rb
@@ -0,0 +1,14 @@
+module VCloud
+  module Constants
+    
+    module Version
+      V0_9 = '0.9' 
+      V1_5 = '1.5'
+      V5_1 = '5.1'
+    end    
+    
+    LOGIN_PATH = '/api/login'
+    SESSION_PATH = '/api/sessions'
+    
+  end
+end
