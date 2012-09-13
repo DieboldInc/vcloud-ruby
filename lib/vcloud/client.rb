c7f366a1455519056d51a7ccc51225232c502081
Zach Robinson <robinsz@diebold.com>
2012-09-13 17:59:05 -0400

    start implementing get_org_refs_by_name

--- lib/vcloud/client.rb
+++ lib/vcloud/client.rb
@@ -44,7 +44,21 @@ module VCloud
   
     end
   
-    
+    def get_org_refs_by_name()
+      #TODO: update verify ssl for prod
+      request = RestClient.Request.new(
+        :url = @links.select {|l| l.type == VCloud::Constants::ContentType::ORG_LIST}.first,
+        :method = 'get',
+        :verify_ssl => false,
+        :headers => @token.merge({:accept => VCloud::Constants::ContentType::ORG_LIST}))
+      response = request.execute
+      doc = Nokogiri::XML(response.body)
+      refs = {}
+      doc.xpath('//xmlns:Org').collect do |elem|
+        refs[elem.attr('name')] = elem
+      end
+      refs    
+    end
   
   
     private
