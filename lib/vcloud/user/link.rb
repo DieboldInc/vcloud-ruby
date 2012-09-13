6516aea995300a9daf6dfd423d45349d43145320
Zach Robinson <robinsz@diebold.com>
2012-09-13 16:21:08 -0400

    remove namespace

--- lib/vcloud/user/link.rb
+++ lib/vcloud/user/link.rb
@@ -1,25 +1,21 @@
 module VCloud
-  module User
-    class Link
-      
-      attr_reader :rel, :type, :href
-      
-      def initialize(args)
-        @rel = args[:rel]
-        @type = args[:type]
-        @href = args[:href]
-      end
-      
-      def self.FromXML(link)
-          doc = XmlSimple.xml_in(link)
-          new(
-            rel: doc['rel'], 
-            type: doc['type'], 
-            href: doc['href']) 
-      end
-      
+  class Link
+
+    attr_reader :rel, :type, :href
+
+    def initialize(args)
+      @rel = args[:rel]
+      @type = args[:type]
+      @href = args[:href]
     end
+
+    def self.FromXML(link)
+      doc = XmlSimple.xml_in(link)
+      new(
+      rel: doc['rel'], 
+      type: doc['type'], 
+      href: doc['href']) 
+    end
+
   end
 end
-
-
--- lib/vcloud/user/organization.rb
+++ lib/vcloud/user/organization.rb
@@ -1,26 +1,22 @@
-require 'xmlsimple'
-
 module VCloud
-  module User
-    class Organization
-      
-      attr_reader :type, :name, :href
-      
-      def initialize(args)
-        @type = args[:type]
-        @name = args[:name]
-        @href = args[:href]
-        
-      end
-      
-      def self.FromXML(org_xml)
-        doc = XmlSimple.xml_in(org_xml)
-        new(
-          type: doc['type'], 
-          name: doc['name'], 
-          href: doc['href'])
-      end
-      
+  class Organization
+
+    attr_reader :type, :name, :href
+
+    def initialize(args)
+      @type = args[:type]
+      @name = args[:name]
+      @href = args[:href]
+
     end
+
+    def self.FromXML(org_xml)
+      doc = XmlSimple.xml_in(org_xml)
+      new(
+      type: doc['type'], 
+      name: doc['name'], 
+      href: doc['href'])
+    end
+
   end
 end
