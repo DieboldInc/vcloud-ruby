95a371f889b9a316b8d65119f3f0ab1fb24f5ff8
Zach Robinson <robinsz@diebold.com>
2012-09-13 18:09:20 -0400

    fix url

--- lib/vcloud/client.rb
+++ lib/vcloud/client.rb
@@ -46,10 +46,8 @@ module VCloud
   
     def get_org_refs_by_name()
       #TODO: update verify ssl for prod
-      url = @links.select {|l| l.type == VCloud::Constants::ContentType::ORG_LIST}.first
-      puts url
       request = RestClient::Request.new(
-        :url => url,
+        :url => @links.select {|l| l.type == VCloud::Constants::ContentType::ORG_LIST}.first.href,
         :method => 'get',
         :verify_ssl => false,
         :headers => @token.merge({:accept => VCloud::Constants::ContentType::ORG_LIST}))
