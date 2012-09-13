fd670061bf1215e86ae45766d16da705a4eb5620
Zach Robinson <robinsz@diebold.com>
2012-09-13 14:31:48 -0400

    create link class

---
+++ lib/vcloud/user/link.rb
@@ -0,0 +1,27 @@
+require 'xmlsimple'
+
+module VCloud
+  module User
+    class Link
+      
+      attr_reader :rel, :type, :href
+      
+      def initialize(args)
+        @rel = args[:rel]
+        @type = args[:type]
+        @href = args[:href]
+      end
+      
+      def self.FromXML(link)
+          doc = XmlSimple.xml_in(link)
+          new(
+            rel: doc['rel'], 
+            type: doc['type'], 
+            href: doc['href']) 
+      end
+      
+    end
+  end
+end
+
+
