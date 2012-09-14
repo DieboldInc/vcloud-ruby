c8dad7fce2a062df90c85df9feab99cb6c720ced
Brian McClain <brian.mcclain@diebold.com>
2012-09-14 17:04:20 -0400

    Began working on pulling Catalogs by ref

--- lib/vcloud/user/org.rb
+++ lib/vcloud/user/org.rb
@@ -2,16 +2,16 @@ module VCloud
   class Org
     include ParsesXml
 
-    attr_reader :type, :name, :href, :vdcs, :catalogs, :networks
+    attr_reader :type, :name, :href, :vdcs, :catalogs, :networks, :vdc_links, :catalog_links, :org_network_links
 
     def initialize(args)
       @type = args[:type]
       @name = args[:name]
       @href = args[:href]
       
-      @vdcs = []
-      @catalogs = []
-      @networks = []
+      @vdc_links = args[:vdc_links]
+      @catalog_links = args[:catalog_links]
+      @org_network_links = args[:org_network_links]
     end
 
     def self.from_reference(ref, session=current_session)
@@ -25,10 +25,14 @@ module VCloud
         :headers => session.token.merge({ :accept => VCloud::Constants::ContentType::ORG+';version=#{session.api_version}' })
       )
 
-      response = request.execute      
-      vdc_links = get_vdc_links(response.body)
+      response = request.execute
+            
+      vdc_links = get_vdc_links(response.body)      
       catalog_links = get_catalog_links(response.body)
-      org_network_links = get_org_network_links(response.body)      
+      org_network_links = get_org_network_links(response.body)
+      
+      #TODO: Pull this from the XML
+      Org.new({:type => VCloud::Constants::ContentType::ORG, :name => "blah", :href => "dah", :vdc_links => vdc_links, :catalog_links => catalog_links, :org_network_links => org_network_links})     
     end
     
     def self.get_vdc_links(xml)
@@ -67,5 +71,18 @@ module VCloud
       return networks
     end
     
+    def get_catalog_links_by_name()
+      catalog_refs = {}
+      @catalog_links.each do |catalog_link|
+        catalog_refs[catalog_link.name] = catalog_link
+      end
+      catalog_refs
+    end
+    
+    def get_catalog_from_name(name)
+      catalogs = get_catalog_links_by_name
+      link = catalogs[name]
+      Catalog.from_reference(link)
+    end
   end
 end
