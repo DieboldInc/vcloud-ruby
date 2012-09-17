56fdd2a9cfa920e22a34daa6a3b3e01f441ee579
Brian McClain <brian.mcclain@diebold.com>
2012-09-17 16:28:36 -0400

    Refactored parses xml, fixed org, began parsing catalogs

--- lib/vcloud/user/catalog.rb
+++ lib/vcloud/user/catalog.rb
@@ -1,35 +1,31 @@
 module VCloud
   class Catalog
     include ParsesXml
+    include RestApi
+    include Session
 
     has_links
-    has_reference :catalog, VCloud::Constants::Xpath::CATALOG_REFERENCE
-    has_reference :catalogItem, VCloud::Constants::Xpath::CATALOG_ITEM_REFERENCE
-
-    attr_reader :name, :id, :type, :href, :catalog_items
+    has_reference :catalog_item_references, VCloud::Constants::Xpath::CATALOG_ITEM_REFERENCE
+    has_default_attributes
+    
+    def self.type
+      VCloud::Constants::ContentType::CATALOG
+    end
     
     def initialize(args)
       @name = args[:name]
       @id = args[:id]
       @type = args[:type]
       @href = args[:href]
-      
+
       @catalog_items = []
     end
     
-    def self.from_reference(ref, session=@@current_session)
-      url = ref.href 
-      
-      #TODO: verify_ssl proper for prod
-      request = RestClient::Request.new(
-        :url => url,
-        :method => 'get',
-        :verify_ssl => false,
-        :headers => session.token.merge({ :accept => VCloud::Constants::ContentType::Catalog+';version=#{session.api_version}' })
-      )
-
-      response = request.execute
-      puts response
+    def self.from_reference(ref, session = VCloud::Session.current_session)
+      obj = new({:href => ref.href})
+      obj.refresh    
+      obj
     end
+
   end
 end
