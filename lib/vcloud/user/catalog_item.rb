6a8f2c89d4d10e14fc91ef7a9a75985040b39222
Zach Robinson <robinsz@diebold.com>
2012-09-18 16:56:08 -0400

    only has one ref, make method to reflect that

--- lib/vcloud/user/catalog_item.rb
+++ lib/vcloud/user/catalog_item.rb
@@ -7,5 +7,9 @@ module VCloud
     has_reference :entity_references, VCloud::Constants::Xpath::ENTITY_REFERENCE
     has_default_attributes
     
+    def entity_reference
+      @entity_references.first
+    end
+    
   end
 end
