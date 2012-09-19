b0ca75e8f8df1648de8c81ee2b090b9b632ed4b5
Brian McClain <brian.mcclain@diebold.com>
2012-09-19 09:29:55 -0400

    Added test for retriving a list of orginizations

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 include WebMock::API
 
 describe VCloud::Client do
-  before(:each) do
-        
+  before(:all) do
+    @test_session_url = 'https://some.vcloud.com/api/sessions' 
+    @test_api_version = '1.5'
+    
+    @session = VCloud::Client.new('https://some.vcloud.com/api/', '1.5')
   end
   
-  it "returns a session" do
-    test_session_url = 'https://some.vcloud.com/api/sessions' 
-    test_api_version = '1.5'
+  it "creates a session" do
     session_xml  = %q{<?xml version="1.0" encoding="UTF-8"?>
     <Session xmlns="http://www.vmware.com/vcloud/v1.5" user="test" org="someorg" type="application/vnd.vmware.vcloud.session+xml" href="https://some.vcloud.com/api/session/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://10.9.225.10/api/v1.5/schema/master.xsd">
         <Link rel="down" type="application/vnd.vmware.vcloud.orgList+xml" href="https://some.vcloud.com/api/org/"/>
@@ -19,12 +20,29 @@ describe VCloud::Client do
     </Session>}
     
     stub_request(:post, "https://test%40someorg:password@some.vcloud.com/api/sessions").
-             with(:headers => {'Accept'=>'application/*+xml;version=#{@api_version}', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
+             with(:headers => {'Accept'=>'application/*+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
              to_return(:status => 200, :body => session_xml, :headers => {:x_vcloud_authorization => "abc123xyz"})
+             
+    @session.login('test@someorg', 'password')
+    VCloud::Session.set_session(@session)
+  end
+  
+  it "retrieves a list of orginizations" do
+    test_session_url = 'https://some.vcloud.com/api/org'
+    test_org_name = "someorg"
+    org_list_xml = %q{<?xml version="1.0" encoding="UTF-8"?>
+    <OrgList xmlns="http://www.vmware.com/vcloud/v1.5" type="application/vnd.vmware.vcloud.orgList+xml" href="https://some.vcloud.com/api/org/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://127.0.0.1/api/v1.5/schema/master.xsd">
+        <Org type="application/vnd.vmware.vcloud.org+xml" name="someorg" href="https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff"/>
+    </OrgList>}
+
+    stub_request(:get, "https://some.vcloud.com/api/org/").
+             with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.orgList+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'}).
+             to_return(:status => 200, :body => org_list_xml, :headers => {})
     
-    session = VCloud::Client.new('https://some.vcloud.com/api/', '1.5')
-    session.login('test@someorg', 'password')
+    org_refs = @session.get_org_refs
+   
+    org_refs.should have(1).items
+    org_refs.first.name.should == "someorg"
     
-    WebMock.should have_requested(:post, "https://test%40someorg:password@some.vcloud.com/api/sessions")
   end
 end
