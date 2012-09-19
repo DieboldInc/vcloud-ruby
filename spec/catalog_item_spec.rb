96ed751dabe0cf18692e1d8401a0189ccc57c7fb
Brian McClain <brian.mcclain@diebold.com>
2012-09-19 16:49:13 -0400

    Began catalog item tests

---
+++ spec/catalog_item_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+include WebMock::API
+
+describe VCloud::CatalogItem do
+  
+  before(:each) do
+    stub_request(:get, "https://vcloud.diebold.dev/api/catalogItem/aaa-bbb-ccc-ddd-eee-fff").
+             with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.catalogItem+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'}).
+             to_return(:status => 200, :body => VCloud::Test::Data::CATALOG_ITEM_XML, :headers => {})
+  end
+  
+  it "retrieves a catalog item" do
+    catalog_item_href = "https://vcloud.diebold.dev/api/catalogItem/aaa-bbb-ccc-ddd-eee-fff"
+    catalog_item = VCloud::CatalogItem.from_reference(VCloud::Reference.new({:href => catalog_item_href}), @session)
+    
+    WebMock.should have_requested(:get, catalog_item_href).
+      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.catalogItem+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
+    
+    puts catalog_item.inspect
+  end  
+end
