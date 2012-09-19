2b887c44febe9d4875c58511a395f9793db3b67d
Zach Robinson <robinsz@diebold.com>
2012-09-19 09:18:36 -0400

    add vdc to org

---
+++ lib/vcloud/user/instantiate_vapp_template_params.rb
@@ -0,0 +1,35 @@
+module VCloud
+  class InstantiateVAppTemplateParams
+
+    attr_accessor :name, :deploy, :power_on, :description, :source, :network_configs, :vapp_template_href
+
+    def initialize
+      @network_configs = []
+      @deploy = true
+      @power_on = true
+    end
+    
+    def to_xml
+      builder = Nokogiri::XML::Builder.new(:encoding => VCloud::Constants::XML_ENCODING) do |xml|
+        xml.InstantiateVAppTemplateParams(:name => @name, :deploy => @deploy, :powerOn => @power_on) {
+          xml.Description @description
+          xml.InstantiationParams {
+           xml.NetworkConfigSection {
+             @network_configs.each do |network|
+               xml.NetworkConfig(:networkName => network.name) {
+                 xml.Configuration {
+                   xml.ParentNetwork(:href => network.href)
+                   xml.FenceMode 'bridged'
+                 }
+               }
+             end
+           } 
+          }
+          xml.Source(:href => @vapp_template_href)
+        }
+      end
+      builder.to_xml
+    end
+
+  end
+end
