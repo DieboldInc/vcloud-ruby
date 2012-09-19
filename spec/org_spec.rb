3ecc85647169bee6b3edb6983df897ac8e2fddb8
Brian McClain <brian.mcclain@diebold.com>
2012-09-19 11:32:37 -0400

    Pulled sample data from class-specific specs to a seperate module, began org tests

---
+++ spec/org_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+include WebMock::API
+
+describe VCloud::Org do
+  
+  before(:all) do
+    
+  end
+  
+  it "retrieves an orginization" do    
+    stub_request(:get, "https://some.vcloud.com/api/org/").
+             with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.orgList+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'}).
+             to_return(:status => 200, :body => VCloud::Test::Data::ORG_LIST_XML, :headers => {})
+    stub_request(:get, "https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff").
+            with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.org+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'}).
+            to_return(:status => 200, :body => VCloud::Test::Data::ORG_XML, :headers => {})
+    
+    org = VCloud::Org.from_reference(VCloud::Reference.new({:href => "https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff"}))
+  end
+  
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,34 @@
 require 'webmock/rspec'
 
 require_relative '../lib/vcloud'
+
+module VCloud
+  module Test
+    module Data
+      SESSION_XML  = %q{<?xml version="1.0" encoding="UTF-8"?>
+      <Session xmlns="http://www.vmware.com/vcloud/v1.5" user="test" org="someorg" type="application/vnd.vmware.vcloud.session+xml" href="https://some.vcloud.com/api/session/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://10.9.225.10/api/v1.5/schema/master.xsd">
+          <Link rel="down" type="application/vnd.vmware.vcloud.orgList+xml" href="https://some.vcloud.com/api/org/"/>
+          <Link rel="down" type="application/vnd.vmware.admin.vcloud+xml" href="https://some.vcloud.com/api/admin/"/>
+          <Link rel="down" type="application/vnd.vmware.vcloud.query.queryList+xml" href="https://some.vcloud.com/api/query"/>
+          <Link rel="entityResolver" type="application/vnd.vmware.vcloud.entity+xml" href="https://some.vcloud.com/api/entity/"/>
+      </Session>}
+      
+      ORG_LIST_XML = %q{<?xml version="1.0" encoding="UTF-8"?>
+      <OrgList xmlns="http://www.vmware.com/vcloud/v1.5" type="application/vnd.vmware.vcloud.orgList+xml" href="https://some.vcloud.com/api/org/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://127.0.0.1/api/v1.5/schema/master.xsd">
+          <Org type="application/vnd.vmware.vcloud.org+xml" name="someorg" href="https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff"/>
+      </OrgList>}
+      
+      ORG_XML = %q{<?xml version="1.0" encoding="UTF-8"?>
+      <Org xmlns="http://www.vmware.com/vcloud/v1.5" name="someorg" id="urn:vcloud:org:e5c140c9-77ca-49f0-8215-43ccfa16a9b3" type="application/vnd.vmware.vcloud.org+xml" href="https://some.vcloud.com/api/org/e5c140c9-77ca-49f0-8215-43ccfa16a9b3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://10.9.225.10/api/v1.5/schema/master.xsd">
+          <Link rel="down" type="application/vnd.vmware.vcloud.vdc+xml" name="SomeVDC" href="https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff"/>
+          <Link rel="down" type="application/vnd.vmware.vcloud.tasksList+xml" href="https://some.vcloud.com/api/tasksList/aaa-bbb-ccc-ddd-eee-fff"/>
+          <Link rel="down" type="application/vnd.vmware.vcloud.catalog+xml" name="SuperCool Catalog" href="https://some.vcloud.com/api/catalog/aaa-bbb-ccc-ddd-eee-fff"/>
+          <Link rel="down" type="application/vnd.vmware.vcloud.controlAccess+xml" href="https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff/catalog/aaa-bbb-ccc-ddd-eee-fff/controlAccess/"/>
+          <Link rel="down" type="application/vnd.vmware.vcloud.orgNetwork+xml" name="Dev VLAN" href="https://some.vcloud.com/api/network/aaa-bbb-ccc-ddd-eee-fff"/>
+          <Link rel="down" type="application/vnd.vmware.vcloud.metadata+xml" href="https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff/metadata"/>
+          <Description>This is an example orginization</Description>
+          <FullName>Example Orginization</FullName>
+      </Org>}
+    end
+  end
+end
