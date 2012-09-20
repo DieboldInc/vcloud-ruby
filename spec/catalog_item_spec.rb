046f0bcf6b517ba07f09c4200152e7d9ff37c6cf
Brian McClain <brian.mcclain@diebold.com>
2012-09-20 09:05:41 -0400

    More tests for catalog item

--- spec/catalog_item_spec.rb
+++ spec/catalog_item_spec.rb
@@ -17,6 +17,19 @@ describe VCloud::CatalogItem do
     WebMock.should have_requested(:get, catalog_item_href).
       with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.catalogItem+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
     
-    puts catalog_item.inspect
+    catalog_item.name.should == "Ubuntu 10.04.4 LTS"
+    catalog_item.href.should == "https://some.vcloud.com/api/catalogItem/aaa-bbb-ccc-ddd-eee-fff"
+    catalog_item.links.should have(2).items
   end  
+  
+  it "retrieves a catalog item entity reference" do
+    catalog_item_href = "https://vcloud.diebold.dev/api/catalogItem/aaa-bbb-ccc-ddd-eee-fff"
+    catalog_item = VCloud::CatalogItem.from_reference(VCloud::Reference.new({:href => catalog_item_href}), @session)
+    
+    WebMock.should have_requested(:get, catalog_item_href).
+      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.catalogItem+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
+    
+    catalog_item.entity_reference.name.should == "Ubuntu 10.04.4 LTS"  
+    catalog_item.entity_reference.href.should == "https://some.vcloud.com/api/vAppTemplate/vappTemplate-aaa-bbb-ccc-ddd-eee-fff"
+  end
 end
