f2bce5f6102977872ef4aa15c6d5d28ba3a97c8a
Brian McClain <brian.mcclain@diebold.com>
2012-09-21 16:45:34 -0400

    Code and tests to parse a VAapp from XML

--- lib/vcloud/user/vapp.rb
+++ lib/vcloud/user/vapp.rb
@@ -7,5 +7,16 @@ module VCloud
     has_default_attributes
     has_tasks
     
+    def self.from_xml(xml)
+      doc = parse_xml(xml)
+      obj = new(
+        name: doc[:name],
+        links: doc[:links],
+        type: doc[:type],
+        href: doc[:href],
+        id: doc[:id],
+        tasks: doc[:tasks],
+      )
+    end
   end
 end
