faf78f791d3ac21f6106cde16665d1162a6e67cb
Brian McClain <brian.mcclain@diebold.com>
2012-09-21 16:45:34 -0400

    Code and tests to parse a VAapp from XML

--- spec/vapp_spec.rb
+++ spec/vapp_spec.rb
@@ -19,6 +19,28 @@ describe VCloud::VApp do
     vapp.links.should have(6).items
   end
   
+  it "can be parsed from XML" do
+    vapp = VCloud::VApp.from_xml(VCloud::Test::Data::VAPP_XML)
+    
+    vapp.name.should == "Linux FTP server"
+    vapp.href.should == "https://some.vcloud.com/api/vApp/vapp-aaa-bbb-ccc-ddd-eee-fff"
+    vapp.links.should have(6).items
+    
+    vapp.tasks.should_not == nil
+    vapp.tasks.should have(1).items
+    vapp.tasks.first.status.should == 'running'
+    vapp.tasks.first.operation_name.should == 'vdcInstantiateVapp'
+    vapp.tasks.first.operation.should == 'Creating Virtual Application Linux FTP server(aaa-bbb-ccc-ddd-eee-fff)'
+    vapp.tasks.first.expiry_time.should == '2012-12-19T10:40:08.286-05:00'
+    vapp.tasks.first.name.should == 'task'
+    vapp.tasks.first.id.should == 'urn:vcloud:task:aaa-bbb-ccc-ddd-eee-fff'
+    vapp.tasks.first.href.should == 'https://some.vcloud.com/api/task/aaa-bbb-ccc-ddd-eee-fff'
+    
+    vapp.tasks.first.links.should have(1).item
+    vapp.tasks.first.links.first.rel.should == 'task:cancel'
+    vapp.tasks.first.links.first.href.should == 'https://some.vcloud.com/api/task/aaa-bbb-ccc-ddd-eee-fff/action/cancel'
+  end
+  
   it "parses tasks" do
     vapp_href = "https://some.vcloud.com/api/vApp/vapp-aaa-bbb-ccc-ddd-eee-fff"
     vapp = VCloud::VApp.from_reference(VCloud::Reference.new({:href => vapp_href}))
