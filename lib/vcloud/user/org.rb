7359fe34f55f98b272c70cbd664545c8f7605910
Zach Robinson <robinsz@diebold.com>
2012-09-26 17:28:43 -0400

    Org is specced and updated for happymapper

--- lib/vcloud/user/org.rb
+++ lib/vcloud/user/org.rb
@@ -1,11 +1,12 @@
 module VCloud
   class Org < BaseVCloudEntity
-    include ParsesXml
     
     has_type VCloud::Constants::ContentType::ORG
     tag 'Org'
     has_links
     has_default_attributes
+    element :description, String, :tag => "Description"
+    element :full_name, String, :tag => "FullName"
     
     def vdc_links
       @links.select {|l| l.type == VCloud::Constants::ContentType::VDC}
@@ -19,22 +20,23 @@ module VCloud
       @links.select {|l| l.type == VCloud::Constants::ContentType::ORG_NETWORK}
     end
     
-    def get_catalog_links_by_name()
+    def get_catalog_links_by_name
       Hash[catalog_links.collect { |l| [l.name, l] }]
     end
     
     def get_catalog_from_name(name)
       catalogs = get_catalog_links_by_name
-      link = catalogs[name]
+      link = catalogs[name] or return nil
       Catalog.from_reference(link)
     end
     
-    def get_vdc_links_by_name()
-      Hash[vdc_links.collect { |l| [l.name, l] }]    end
+    def get_vdc_links_by_name
+      Hash[vdc_links.collect { |l| [l.name, l] }]
+    end
     
     def get_vdc_from_name(name)
       links = get_vdc_links_by_name
-      link = links[name]
+      link = links[name] or return nil
       Vdc.from_reference(link)
     end    
   end
--- spec/org_spec.rb
+++ spec/org_spec.rb
@@ -3,62 +3,97 @@ require 'spec_helper'
 include WebMock::API
 
 describe VCloud::Org do
-  
   before(:each) do
+    @org = VCloud::Org.from_xml(fixture_file('org.xml'))
+  end
+
+  describe 'parses xml #from_xml' do    
+    it 'should have correct values' do
+      @org.name.should == 'someorg'                      
+      @org.id.should == 'urn:vcloud:org:aaa-bbb-ccc-ddd-eee-fff'
+      @org.type.should == 'application/vnd.vmware.vcloud.org+xml'
+      @org.href.should == 'https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff'
+      @org.description.should == 'This is an example organization'
+      @org.full_name.should == 'Example Organization'
+      @org.links.should have(6).items
+    end
     
-    # Org List stub
-    stub_request(:get, "https://some.vcloud.com/api/org/").
-             with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.orgList+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'}).
-             to_return(:status => 200, :body => VCloud::Test::Data::ORG_LIST_XML, :headers => {})
-             
-    # Org stub
+    it 'should filter #vdc_links' do
+      @org.vdc_links.should have(1).item
+      @org.vdc_links[0].type.should == "application/vnd.vmware.vcloud.vdc+xml"
+    end
+    
+    it 'should filter #catalog_links' do
+      @org.catalog_links.should have(1).item
+      @org.catalog_links[0].type.should == "application/vnd.vmware.vcloud.catalog+xml"
+    end
+    
+    it 'should filter #org_network_links' do
+      @org.org_network_links.should have(1).item
+      @org.org_network_links[0].type.should == "application/vnd.vmware.vcloud.orgNetwork+xml"      
+    end
+    
+    it 'should #get_catalog_links_by_name' do
+      hash = @org.get_catalog_links_by_name
+
+      hash.should have(1).items
+      hash['SuperCool Catalog'].href.should == 'https://some.vcloud.com/api/catalog/aaa-bbb-ccc-ddd-eee-fff'
+    end     
+    
+    it 'should #get_vdc_links_by_name' do
+      hash = @org.get_vdc_links_by_name
+      
+      hash.should have(1).items
+      hash['SomeVDC'].href.should == 'https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff'
+    end                                                                                      
+  end
+  
+  it 'should #get_catalog_from_name if name exists' do
+    link = @org.get_catalog_links_by_name['SuperCool Catalog']
+    VCloud::Catalog.should_receive(:from_reference).with(link).and_return('not nil')
+    
+    catalog = @org.get_catalog_from_name('SuperCool Catalog')
+    
+    catalog.should_not be_nil
+  end
+
+  it 'should return nil #get_catalog_from_name if name does not exist' do
+    catalog = @org.get_catalog_from_name('no exist')
+    
+    catalog.should be_nil
+  end   
+  
+  it 'should #get_vdc_from_name if name exists' do
+    link = @org.get_vdc_links_by_name['SomeVDC']
+    VCloud::Vdc.should_receive(:from_reference).with(link).and_return('not nil')
+    
+    vdc = @org.get_vdc_from_name('SomeVDC')
+    
+    vdc.should_not be_nil
+  end
+  
+  it 'should return nil #get_vdc_from_name if name does not exist' do
+    vdc = @org.get_vdc_from_name('no exist')
+    
+    vdc.should be_nil
+  end  
+  
+  it 'should retrieve organization #from_reference' do
     stub_request(:get, "https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff").
-            with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.org+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'}).
-            to_return(:status => 200, :body => VCloud::Test::Data::ORG_XML, :headers => {})
-  end
-  
-  it "retrieves an orginization" do    
-    org_href = "https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff"
-    org = VCloud::Org.from_reference(VCloud::Reference.new({:href => org_href}), @session)
+      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.org+xml;version=1.5', 'X-Vcloud-Authorization'=>'abc123xyz'}).
+      to_return(:status => 200, :body => fixture_file('org.xml'))  
     
-    WebMock.should have_requested(:get, org_href).
-      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.org+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
+    org = VCloud::Org.from_reference(stub(:href => 'https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff'), @session)
     
-    org.should_not == nil
-    org.links.should_not == nil
-    org.id.should_not == nil
-    org.type.should_not == nil
-    org.name.should_not == nil
-    org.href.should_not == nil
-    org.vdc_links.should have(1).items
-    org.catalog_links.should have(1).items
-    org.org_network_links.should have(1).items
-  end
-  
-  it "parses VDC links" do
-    org_href = "https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff"
-    org = VCloud::Org.from_reference(VCloud::Reference.new({:href => org_href}), @session)
-    vdc_links = org.get_vdc_links_by_name
-    
-    WebMock.should have_requested(:get, org_href).
-      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.org+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
-    
-    vdc_links.should have(1).item
-    vdc_links["SomeVDC"].name.should == "SomeVDC"
-    vdc_links["SomeVDC"].href.should == "https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff"
-  end
-  
-  it "parses Catalog links" do
-    org_href = "https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff"
-    org = VCloud::Org.from_reference(VCloud::Reference.new({:href => org_href}), @session)
-    catalog_links = org.get_catalog_links_by_name
-    
-    WebMock.should have_requested(:get, org_href).
-      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.org+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
-    
-    catalog_links.should have(1).item
-    catalog_links["SuperCool Catalog"].name.should == "SuperCool Catalog"
-    catalog_links["SuperCool Catalog"].href.should == "https://some.vcloud.com/api/catalog/aaa-bbb-ccc-ddd-eee-fff"
-  end
-  
-end
+    WebMock.should have_requested(:get, 'https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff').
+      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.org+xml;version=1.5', 'X-Vcloud-Authorization'=>'abc123xyz'})
+
+    org.name.should == 'someorg'                      
+    org.id.should == 'urn:vcloud:org:aaa-bbb-ccc-ddd-eee-fff'
+    org.type.should == 'application/vnd.vmware.vcloud.org+xml'
+    org.href.should == 'https://some.vcloud.com/api/org/aaa-bbb-ccc-ddd-eee-fff'
+    org.description.should == 'This is an example organization'
+    org.full_name.should == 'Example Organization'
+    org.links.should have(6).items
+  end   
+end    
