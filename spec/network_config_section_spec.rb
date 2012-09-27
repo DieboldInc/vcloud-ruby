b3a82f31f751236f777c13603150acf26637b819
Zach Robinson <robinsz@diebold.com>
2012-09-27 14:55:20 -0400

    cleanup network_config_section and specs

--- spec/network_config_section_spec.rb
+++ spec/network_config_section_spec.rb
@@ -3,46 +3,40 @@ require 'spec_helper'
 include WebMock::API
 
 describe VCloud::NetworkConfigSection do
-  
-  before(:each) do
-    
+
+  it 'should #initialize' do
+    net_section = VCloud::NetworkConfigSection.new
+    net_section.network_configs.should have(0).items
+    net_section.info.should == 'Configuration parameters for logical networks'
   end
   
-  it "creates a new NetworkConfigSection" do
-    net_section = VCloud::NetworkConfigSection.new
-    net_section.network_configurations.should have(0).items
-  end
-  
-  it "adds a VAppNetworkConfig" do
-    net_config = VCloud::VAppNetworkConfig.new
-    net_config.network_name = "TestVappNetworkConfigNetwork"
-    net_config.parent_network_ref = VCloud::Reference.new({})
-    net_config.fence_mode = "bridged"
+  describe 'when parsing #from_xml' do
+    before(:each) do
+      @net_section = VCloud::NetworkConfigSection.from_xml(fixture_file('network_config_section.xml'))
+    end
     
+   it 'should have correct values' do
+     @net_section.href.should == 'https://vcloud.example.dev/api/vAppTemplate/vappTemplate-1/networkConfigSection/'
+     @net_section.type.should == 'application/vnd.vmware.vcloud.networkConfigSection+xml'
+     @net_section.info.should == 'The configuration parameters for logical networks'
+     @net_section.network_configs.should have(1).items
+   end
+ end  
+
+  it "should serialize #to_xml" do      
     net_section = VCloud::NetworkConfigSection.new
-    net_section.network_configurations << net_config
-    
-    net_section.network_configurations.should have(1).item
-    net_section.network_configurations.first.network_name.should == "TestVappNetworkConfigNetwork"
-    net_section.network_configurations.first.fence_mode.should == "bridged"
-  end
-  
-  it "seralizes to XML" do
-    # net_config = VCloud::VAppNetworkConfig.new
-    net_config = VCloud::NetworkConfiguration.new
-    net_config.network_name = "TestVappNetworkConfigNetwork"
-    # TODO: Actually make a parent network
-    net_config.parent_network = VCloud::Reference.new({})
-    net_config.fence_mode = "bridged"
-    
-    net_section = VCloud::NetworkConfigSection.new
-    net_section.network_configurations << net_config
+    net_section.href = 'http://some.href.com'    
+    net_section.type = 'some type'
+    net_section.info = 'info text'
+    net_section.network_configs << "net section text"
     
     xml = net_section.to_xml
     doc = Nokogiri::XML(xml)
- 
-    doc.at_xpath('/xmlns:NetworkConfigSection/xmlns:NetworkConfiguration')['networkName'].should == "TestVappNetworkConfigNetwork"
-    doc.at_xpath('/xmlns:NetworkConfigSection/xmlns:NetworkConfiguration/xmlns:FenceMode').text.should == "bridged"
-  end
+
+    doc.xpath('/xmlns:NetworkConfigSection/@href').text.should == "http://some.href.com"
+    doc.xpath('/xmlns:NetworkConfigSection/@type').text.should == "some type"
+    doc.xpath('/xmlns:NetworkConfigSection/ovf:Info').text.should == "info text"
+    doc.xpath('/xmlns:NetworkConfigSection/xmlns:NetworkConfig').text.should == "net section text"
+  end   
   
 end
