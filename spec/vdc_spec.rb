82ddf8e9cad0316df249b352e903b4d477157f99
Brian McClain <brian.mcclain@diebold.com>
2012-09-27 11:15:33 -0400

    Update VDC tests

--- spec/vdc_spec.rb
+++ spec/vdc_spec.rb
@@ -3,49 +3,41 @@ require 'spec_helper'
 include WebMock::API
 
 describe VCloud::Vdc do
-  
   before(:each) do
+    @vdc = VCloud::Vdc.from_xml(fixture_file('vdc.xml'))    
+  end
+  
+  describe 'when parsing xml #from_xml' do 
+    it 'should have correct values' do
+      @vdc.name.should == 'TestVDC'
+      @vdc.id.should == 'urn:vcloud:vdc:aaa-bbb-ccc-ddd-eee-fff'
+      @vdc.type.should == 'application/vnd.vmware.vcloud.vdc+xml'
+      @vdc.href.should == 'https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff'
+      @vdc.links.should have(10).items
+      @vdc.network_references.should have(1).items
+    end
+    
+    it "should parese a network references" do  
+      @vdc.network_references.should have(1).items
+      @vdc.network_references.first.name.should == "Dev VLAN"
+      @vdc.network_references.first.href.should == "https://some.vcloud.com/api/network/aaa-bbb-ccc-ddd-eee-fff"
+    end
+  end
+  
+  it "should retrieve VDC #from_reference" do
     stub_request(:get, "https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff").
-             with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.vdc+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'}).
-             to_return(:status => 200, :body => VCloud::Test::Data::VDC_XML, :headers => {})
+      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.vdc+xml;version=1.5', 'X-Vcloud-Authorization'=>'abc123xyz'}).
+      to_return(:status => 200, :body => fixture_file('vdc.xml'))
     
+    vdc = VCloud::Vdc.from_reference(stub(:href => 'https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff'), @session)
     
-    stub_request(:post, "https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff/action/instantiateVAppTemplate").
-             with({'Accept'=>'application/*+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/vnd.vmware.vcloud.instantiateVAppTemplateParams+xml', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'}).
-             to_return(:status => 200, :body => VCloud::Test::Data::VAPP_XML, :headers => {})
+    WebMock.should have_requested(:get, 'https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff').
+      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.vdc+xml;version=1.5', 'X-Vcloud-Authorization'=>'abc123xyz'})
+        
+    vdc.id.should == "urn:vcloud:vdc:aaa-bbb-ccc-ddd-eee-fff"
   end
   
-  it "retrieves a VDC" do
-    vdc_href = "https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff"
-    vdc = VCloud::Vdc.from_reference(VCloud::Reference.new({:href => vdc_href}), @session)
-    
-    WebMock.should have_requested(:get, vdc_href).
-      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.vdc+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
-    
-    vdc.name.should == "TestVDC"
-    vdc.href.should == "https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff"
-    vdc.links.should have(10).items
-  end
-  
-  it "pareses a network references" do
-    vdc_href = "https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff"
-    vdc = VCloud::Vdc.from_reference(VCloud::Reference.new({:href => vdc_href}), @session)
-    
-    WebMock.should have_requested(:get, vdc_href).
-      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.vdc+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
-    
-    vdc.network_references.should have(1).item
-    vdc.network_references.first.name.should == "Dev VLAN"
-    vdc.network_references.first.href.should == "https://some.vcloud.com/api/network/aaa-bbb-ccc-ddd-eee-fff"
-  end
-  
-  it "instantiates a vapp template" do
-    vdc_href = "https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff"
-    vapp_href = "https://some.vcloud.com/api/vApp/vapp-aaa-bbb-ccc-ddd-eee-fff"
-    instantiate_vapp_template_url = "https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff/action/instantiateVAppTemplate"
-    
-    vdc = VCloud::Vdc.from_reference(VCloud::Reference.new({:href => vdc_href}), @session)
-    
+  it "should #instantiate_vapp_template" do    
     net_section = VCloud::NetworkConfigSection.new
     net_config = VCloud::VAppNetworkConfig.new
     net_config.network_name = "TestVappNetworkConfigNetwork"
@@ -59,15 +51,16 @@ describe VCloud::Vdc do
     vapp_params.source = VCloud::Reference.new({})
     vapp_params.instantiation_param_items << net_section
     
-    result = vdc.instantiate_vapp_template(vapp_params)
+    stub_request(:post, 'https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff/action/instantiateVAppTemplate').
+      with(:headers => {'Accept'=>'application/*+xml;version=1.5', 'Content-Type'=>'application/vnd.vmware.vcloud.instantiateVAppTemplateParams+xml', 'X-Vcloud-Authorization'=>'abc123xyz'}).
+      to_return(:status => 200, :body => fixture_file('vapp.xml'))
     
-    WebMock.should have_requested(:get, vdc_href).
-      with(:headers => {'Accept'=>'application/vnd.vmware.vcloud.vdc+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
-    WebMock.should have_requested(:post, instantiate_vapp_template_url).
-      with(:headers => {'Accept'=>'application/*+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/vnd.vmware.vcloud.instantiateVAppTemplateParams+xml', 'User-Agent'=>'Ruby', 'X-Vcloud-Authorization'=>'abc123xyz'})
+    vapp = @vdc.instantiate_vapp_template(vapp_params)
+    
+    WebMock.should have_requested(:post, 'https://some.vcloud.com/api/vdc/aaa-bbb-ccc-ddd-eee-fff/action/instantiateVAppTemplate').
+      with(:headers => {'Accept'=>'application/*+xml;version=1.5', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/vnd.vmware.vcloud.instantiateVAppTemplateParams+xml', 'X-Vcloud-Authorization'=>'abc123xyz'})
   
-    result.name.should == 'Linux FTP server'
-    result.href.should == vapp_href
+    vapp.id.should == 'urn:vcloud:vapp:aaa-bbb-ccc-ddd-eee-fff'
   end
   
 end
