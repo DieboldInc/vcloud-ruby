9fa34648fad0ba1774c9ca87d5672ddcc6c88855
Zach Robinson <zach.robinson@diebold.com>
2012-10-02 14:40:58 -0400

    justify

--- lib/vcloud/user/instantiate_vapp_template_params.rb
+++ lib/vcloud/user/instantiate_vapp_template_params.rb
@@ -5,16 +5,16 @@ module VCloud
 
     register_namespace 'xmlns', VCloud::Constants::NameSpace::V1_5
     tag 'InstantiateVAppTemplateParams'
-    attribute :name, String
-    attribute :deploy, Boolean
-    attribute :power_on, Boolean, :tag => 'powerOn'
-    element :description, String, :tag => 'Description'
+    attribute :name,        String
+    attribute :deploy,      Boolean
+    attribute :power_on,    Boolean,  :tag => 'powerOn'
+    element   :description, String,   :tag => 'Description'
     wrap 'InstantiationParams' do
       has_one :network_config_section, 'VCloud::NetworkConfigSection', :tag => 'NetworkConfigSection'
     end
-    element :source_reference, 'VCloud::Reference', :tag => 'Source'
-    element :is_source_delete, Boolean, :tag => 'IsSourceDelete'
-    element :all_eulas_accepted, Boolean, :tag => 'AllEULAsAccepted'
+    element :source_reference,    'VCloud::Reference',  :tag => 'Source'
+    element :is_source_delete,    Boolean,              :tag => 'IsSourceDelete'
+    element :all_eulas_accepted,  Boolean,              :tag => 'AllEULAsAccepted'
     
     # A new instance of InstantiateVAppTemplateParams
     #
