74e34bc849b2e8b7b68a69247a59fdacee1a3d6d
Brian McClain <brian.mcclain@diebold.com>
2012-10-03 09:49:10 -0400

    Began rspec tests for RestApi

---
+++ spec/rest_api_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+include WebMock::API
+
+describe VCloud::RestApi do
+  describe 'when receiving an HTTP error code' do
+    it 'should handle a HTTP 404 error when logging in with a bad API endpoint' do
+      stub_request(:post, "https://someuser%40someorg:password@some.vcloud.com/badapiendpoint/sessions").
+               with(:headers => {'Accept'=>'application/*+xml;version=1.5'}).
+               to_return(:status => 404, :body => fixture_file('error_login_404.html'), :headers => {})
+               
+      session = VCloud::Client.new('https://some.vcloud.com/badapiendpoint/', '1.5')             
+      
+      expect { session.login('someuser@someorg', 'password') }.to raise_error(VCloud::VCloudException) { |error| 
+        error.major_error_code.should == 404
+      }
+    end
+  end
+end
